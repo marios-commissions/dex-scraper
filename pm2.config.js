7e0020d5d58ab2a23beca2317399bb40ee3d2e84
Mario P <me@marioparaschiv.com>
2025-01-01 14:25:12 +0000

    🐛  fix: pm2 ports

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	apps: [
 		{ name: 'dex-scraper', script: 'dist/index.js' },
 		{ name: 'dex-scraper-cloud', script: 'start-cloud.sh' },
-		{ name: 'dex-scraper-web', script: 'web/serve.js' },
+		{ name: 'dex-scraper-web', script: 'start-web.sh' },
 	]
 };
